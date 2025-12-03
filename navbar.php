3a79edfaca5b069fcc000c0513d0bd38018918f2
Infinitium4 <145613499+Infinitium4@users.noreply.github.com>
2025-12-03 11:54:11 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -1,22 +1,28 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="connexion.php">Compte</a>
+        <a class="navbar-brand" href="index.php">Accueil</a>
+        <a class="navbar-brand d-lg-none" href="connexion.php" id="SeConnecter">Se Connecter</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto">
                 <?php
-                $DossierAAnalyser = "formation";
+                $DossierAAnalyser = "Formation";
                 $fichiers = scandir($DossierAAnalyser);
                 foreach ($fichiers as $fichier) {
                     if ($fichier != "." && $fichier != "..") {
                         $nom_sans_ext = pathinfo($fichier, PATHINFO_FILENAME);
-                        echo '<li class="nav-item"><a class="nav-link" href="media/FOND D4ECRAN.jpg?d='.$nom_sans_ext.'">'.$nom_sans_ext.'</a></li>';
+                        echo '<li class="nav-item"><a class="nav-link" href="connexion.php?d='.$nom_sans_ext.'">'.$nom_sans_ext.'</a></li>';
                     }
                 }
                 ?>
             </ul>
+
+            <!-- SE CONNECTER A DROITE SUR GRAND ÉCRAN -->
+            <a class="navbar-brand ms-lg-auto" href="connexion.php" id="SeConnecter">Se Connecter</a>
         </div>
     </div>
 </nav>
