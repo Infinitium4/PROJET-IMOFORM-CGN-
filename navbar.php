87d79cd2c0b9f62d9340e1e2e377e80ece642b06
Iooro <pageot.gab1@gmail.com>
2025-12-03 12:36:40 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -1,9 +1,11 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Accueil</a>
+
+        <!-- Bouton mobile -->
         <a class="navbar-brand d-lg-none" href="connexion.php" id="SeConnecter">Se Connecter</a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -22,7 +24,14 @@
             </ul>
 
             <!-- SE CONNECTER A DROITE SUR GRAND ÉCRAN -->
-            <a class="navbar-brand ms-lg-auto" href="connexion.php" id="SeConnecter">Se Connecter</a>
+            <a class="navbar-brand ms-lg-auto d-none d-lg-block" href="connexion.php">Se Connecter</a>
         </div>
     </div>
 </nav>
+#SeConnecter{
+    margin-left: auto;
+}
+
+.center{
+    text-align: center;
+}
