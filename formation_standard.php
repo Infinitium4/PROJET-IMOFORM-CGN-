1238882c7a65a4385ed83a1c46a65c9dc3d9a3b8
Infinitium4 <145613499+Infinitium4@users.noreply.github.com>
2025-12-05 15:54:20 +0100

    aura nils

---
+++ formation_standard.php
@@ -0,0 +1,5 @@
+<?php
+include "header.php";
+include "navbar.php";
+?>
+<a href="demande_standard.php" class="btn" id="bouton_standard">faire une demande</a>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "navbar.php";
 
 <div class="Accueuil">
     <h1>Accueil</h1>
-    <img src="media/logo.png">
+    <img src="media/logo01.png" id="logo">
 </div>
 <?php
 include "footer.php";
