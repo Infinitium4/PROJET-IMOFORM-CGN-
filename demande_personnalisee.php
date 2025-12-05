5a62364ff9cae528facfbd02e42f07c09f16040f
Infinitium4 <145613499+Infinitium4@users.noreply.github.com>
2025-12-05 10:14:09 +0100

    avancement

--- demande_personnalisee.php
+++ demande_personnalisee.php
@@ -27,9 +27,24 @@ include "navbar.php";
             <label class="form-label" for="date_heure">Date & Heure</label>
             <input id="date_heure" type="datetime-local" name="date_heure" class="form-control" required>
         </div>
+        
+        <div class="mb-3">
+            <label class="form-label" for="cout_formation">cout formation</label>
+            <textarea id="description" name="description" class="form-control" rows="4"></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label" for="commentaire">commentaire</label>
+            <textarea id="description" name="description" class="form-control" rows="4"></textarea>
+        </div>
+
+         <div class="mb-3">
+            <label class="form-label" for="lien">lien</label>
+            <textarea id="description" name="description" class="form-control" rows="4"></textarea>
+        </div>
 
         <input type="hidden" name="token" value="<?php echo $token; ?>">
 
-        <button type="submit" class="btn btn-success w-100">OK</button>
+        <button type="submit" class="btn btn-success w-100">envoyer</button>
     </form>
 </div>
