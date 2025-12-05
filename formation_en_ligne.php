ca0f5fd580fc526dd47ac2923d2a93db615ef499
PignoufCoco <joguetcorentin34@gmail.com>
2025-12-05 14:10:40 +0100

    Essaie pour le bouton de la daronne a Nils

--- formation_en_ligne.php
+++ formation_en_ligne.php
@@ -2,10 +2,32 @@
 include "header.php";
 include "navbar.php";
 ?>
-<?php
-$afficherBouton = true; // true si tu veux que le bouton apparaisse
-?>
 
-<?php if ($afficherBouton): ?>
-    <a href="demande_F_ligne.php" class="btn" id="boutonRecurante">faire une demande</a>
-<?php endif; ?>
+<?php
+
+$req = $pdo->prepare("Select type_profile From compte");
+$req->execute();
+$type_profil = $req->fetchAll();
+
+$agences = "select id_agence from travail where id_utilisateur = id_contact";
+$adherants = false;
+
+if ($type_profil != "contact") {
+    echo "Vous ne remplissez pas les critères pour accéder à cette page.";
+} else {
+    foreach (agences as $agence) {
+        $adherants = "select adherant from agence where $agence = id";
+        if ($adherants == true) {
+            echo '<a href="demande_F_ligne.php" class="btn" id="boutonRecurante">faire une demande</a>';
+        }
+    }
+}
+?>
+$req=$pdo->prepare("INSERT INTO produits (Forme, Hauteur_cm, Prix, FixationMurale) VALUES (:Forme, :Hauteur_cm, :Prix, :FixationMurale)");
+$req->bindParam(':Forme', $Forme);
+$req->bindParam(':Hauteur_cm', $Hauteur_cm);
+$req->bindParam(':Prix', $Prix);
+$req->bindParam(':FixationMurale', $FixationMurale);
+
+
+$req->execute();
