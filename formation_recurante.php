e414564ecb82b582721081bc72f06ea6facfc87c
PignoufCoco <joguetcorentin34@gmail.com>
2025-12-05 08:49:01 +0100

    creation formation

--- formation_recurante.php
+++ formation_recurante.php
@@ -1,4 +1,6 @@
 <?php
-include ("header.php");
-include ("navbar.php");
+include "header.php";
+include "navbar.php";
 ?>
+<link rel="stylesheet" href="style.css">
+<a href="demande_recurante.php" class="btn" id="boutonRecurante">faire une demande</a>
